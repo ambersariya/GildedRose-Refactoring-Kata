fd35a1ee5670a8f8f92b1dfb23780f87917cdb87
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-07-21 19:43:07 +0100

    refactor: make tests parameterised

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,78 +2,30 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
+from parameterized import parameterized
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("foo", items[0].name)
+    @parameterized.expand([
+        ('Foo', 0, 0, -1, 0),
+        ('Aged Brie', 1, 1, 0, 2),
+        ('Aged Brie', 10, 50, 9, 50),
+        ('Sulfuras, Hand of Ragnaros', 10, 80, 10, 80),
+        ('Sulfuras, Hand of Ragnaros', -1, 80, -1, 80),
+        ('Backstage passes to a TAFKAL80ETC concert', 15, 10, 14, 11),
+        ('Backstage passes to a TAFKAL80ETC concert', 10, 10, 9, 12),
+        ('Backstage passes to a TAFKAL80ETC concert', 4, 10, 3, 13),
+        ('Backstage passes to a TAFKAL80ETC concert', -1, 10, -2, 0),
+    ])
+    def test_item(self, item_name, sell_in, quality, expected_sell_in, expected_quality):
+        items = [Item(item_name, sell_in=sell_in, quality=quality)]
 
-    def test_aged_brie_increases_in_quality_with_its_sell_in_date(self):
-        items = [Item("Aged Brie", sell_in=1, quality=1)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("Aged Brie", items[0].name)
-        self.assertEquals(0, items[0].sell_in)
-        self.assertEquals(2, items[0].quality)
 
-    def test_aged_brie_doesnt_increases_in_quality_when_its_already_at_max(self):
-        items = [Item("Aged Brie", sell_in=10, quality=50)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Aged Brie", items[0].name)
-        self.assertEquals(9, items[0].sell_in)
-        self.assertEquals(50, items[0].quality)
-
-    def test_not_increase_in_quality_for_sulfuras_within_sell_in_date(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", sell_in=10, quality=80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Sulfuras, Hand of Ragnaros", items[0].name)
-        self.assertEquals(10, items[0].sell_in)
-        self.assertEquals(80, items[0].quality)
-
-    def test_not_increase_in_quality_for_sulfuras_when_not_in_sell_in_date(self):
-        items = [Item("Sulfuras, Hand of Ragnaros", sell_in=-1, quality=80)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Sulfuras, Hand of Ragnaros", items[0].name)
-        self.assertEquals(-1, items[0].sell_in)
-        self.assertEquals(80, items[0].quality)
-
-    def test_backstage_passes_increase_in_value_by_1_when_above_10_days(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=15, quality=10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name)
-        self.assertEquals(14, items[0].sell_in)
-        self.assertEquals(11, items[0].quality)
-
-    def test_backstage_passes_increase_in_value_by_2_when_10_days_or_less(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=10, quality=10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name)
-        self.assertEquals(9, items[0].sell_in)
-        self.assertEquals(12, items[0].quality)
-
-    def test_backstage_passes_increase_in_value_by_3_when_below_5_days(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=4, quality=10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name)
-        self.assertEquals(3, items[0].sell_in)
-        self.assertEquals(13, items[0].quality)
-
-    def test_backstage_passes_drop_quality_to_0_after_sellin_date(self):
-        items = [Item("Backstage passes to a TAFKAL80ETC concert", sell_in=-1, quality=10)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEquals("Backstage passes to a TAFKAL80ETC concert", items[0].name)
-        self.assertEquals(-2, items[0].sell_in)
-        self.assertEquals(0, items[0].quality)
+        self.assertEquals(item_name, items[0].name)
+        self.assertEquals(expected_quality, items[0].quality)
+        self.assertEquals(expected_sell_in, items[0].sell_in)
 
 
 if __name__ == '__main__':
