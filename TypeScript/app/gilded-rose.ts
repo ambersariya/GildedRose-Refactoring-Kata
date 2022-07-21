f07d531b6acd0930415a2f97b7bf3814068b06cf
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-07-21 15:46:46 +0100

    refactor: introduce method to update quality per item

--- TypeScript/app/gilded-rose.ts
+++ TypeScript/app/gilded-rose.ts
@@ -24,55 +24,57 @@ export class GildedRose {
   updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       const item = this.items[i];
-      if (
-        item.name != AGED_BRIE &&
-        item.name != BACKSTAGE_PASSES
-      ) {
-        if (item.quality > 0) {
-          if (item.name != SULFURAS) {
-            item.quality = item.quality - 1;
-          }
-        }
-      } else {
-        if (item.quality < 50) {
-          item.quality = item.quality + 1;
-          if (item.name == BACKSTAGE_PASSES) {
-            if (item.sellIn < 11) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-            if (item.sellIn < 6) {
-              if (item.quality < 50) {
-                item.quality = item.quality + 1;
-              }
-            }
-          }
+      this.updateItemQuality(item);
+    }
+
+    return this.items;
+  }
+
+  private updateItemQuality(item: Item) {
+    if (item.name != AGED_BRIE &&
+      item.name != BACKSTAGE_PASSES) {
+      if (item.quality > 0) {
+        if (item.name != SULFURAS) {
+          item.quality = item.quality - 1;
         }
       }
-      if (item.name != SULFURAS) {
-        item.sellIn = item.sellIn - 1;
-      }
-      if (item.sellIn < 0) {
-        if (item.name != AGED_BRIE) {
-          if (item.name != BACKSTAGE_PASSES) {
-            if (item.quality > 0) {
-              if (item.name != SULFURAS) {
-                item.quality = item.quality - 1;
-              }
+    } else {
+      if (item.quality < 50) {
+        item.quality = item.quality + 1;
+        if (item.name == BACKSTAGE_PASSES) {
+          if (item.sellIn < 11) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
             }
-          } else {
-            item.quality =
-              item.quality - item.quality;
           }
-        } else {
-          if (item.quality < 50) {
-            item.quality = item.quality + 1;
+          if (item.sellIn < 6) {
+            if (item.quality < 50) {
+              item.quality = item.quality + 1;
+            }
           }
         }
       }
     }
-
-    return this.items;
+    if (item.name != SULFURAS) {
+      item.sellIn = item.sellIn - 1;
+    }
+    if (item.sellIn < 0) {
+      if (item.name != AGED_BRIE) {
+        if (item.name != BACKSTAGE_PASSES) {
+          if (item.quality > 0) {
+            if (item.name != SULFURAS) {
+              item.quality = item.quality - 1;
+            }
+          }
+        } else {
+          item.quality =
+            item.quality - item.quality;
+        }
+      } else {
+        if (item.quality < 50) {
+          item.quality = item.quality + 1;
+        }
+      }
+    }
   }
 }
