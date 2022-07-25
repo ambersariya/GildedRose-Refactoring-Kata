e27652e6575f614f46b66f4e280b4fe490181f52
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-07-25 10:37:44 +0100

    tests: add parameterised test cases

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -2,15 +2,31 @@
 import unittest
 
 from gilded_rose import Item, GildedRose
+from parameterized import parameterized
 
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    @parameterized.expand([
+        ('Foo', 0, 0, -1, 0),
+        ('Aged Brie', 1, 1, 0, 2),
+        ('Aged Brie', 10, 50, 9, 50),
+        ('Sulfuras, Hand of Ragnaros', 10, 80, 10, 80),
+        ('Sulfuras, Hand of Ragnaros', -1, 80, -1, 80),
+        ('Backstage passes to a TAFKAL80ETC concert', 15, 10, 14, 11),
+        ('Backstage passes to a TAFKAL80ETC concert', 10, 10, 9, 12),
+        ('Backstage passes to a TAFKAL80ETC concert', 4, 10, 3, 13),
+        ('Backstage passes to a TAFKAL80ETC concert', -1, 10, -2, 0),
+    ])
+    def test_item(self, item_name, sell_in, quality, expected_sell_in, expected_quality):
+        items = [Item(item_name, sell_in=sell_in, quality=quality)]
+
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
 
-        
+        self.assertEquals(item_name, items[0].name)
+        self.assertEquals(expected_quality, items[0].quality)
+        self.assertEquals(expected_sell_in, items[0].sell_in)
+
+
 if __name__ == '__main__':
     unittest.main()
