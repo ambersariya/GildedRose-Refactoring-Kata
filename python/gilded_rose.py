3c782f01879ff3253309469b042013e891c77be2
ambersariya <1080238+ambersariya@users.noreply.github.com>
2022-07-25 11:15:50 +0100

    tests: add more test cases for normal items.

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -7,33 +7,37 @@ class GildedRose(object):
 
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
+            if item.name == "Sulfuras, Hand of Ragnaros":
+                return
+            self.reduce_sell_in(item)
+
+            if item.name == "Aged Brie":
                 if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+                    self.increase_item_quality(item)
+                return
+
+            if item.name == "Backstage passes to a TAFKAL80ETC concert":
+                if item.quality < 50:
+                    self.increase_item_quality(item)
+                if item.sell_in < 11:
+                    self.increase_item_quality(item)
+                if item.sell_in < 6:
+                    self.increase_item_quality(item)
+                if item.sell_in < 0:
+                    item.quality = item.quality - item.quality
+                return
+
+            if item.quality > 0:
+                self.decrease_item_quality(item)
+
+    def reduce_sell_in(self, item):
+        item.sell_in -= 1
+
+    def increase_item_quality(self, item):
+        item.quality += 1
+
+    def decrease_item_quality(self, item):
+        item.quality -= 1
 
 
 class Item:
